e6c8f497bf553eae7be754764c09b0b49b43d912
hvdthong <hvdthong@gmail.com>
2018-10-24 04:59:59 +0000

    adding file

--- ultis.py
+++ ultis.py
@@ -1,2 +1,8 @@
+def load_file(path):
+	with open(path) as f:
+		content = f.readlines()
+	return [x.strip() for x in content]
+
 if __name__ == '__main__':
-	print 'hello'
+	print ('hello')
+	print ('testing vim')
