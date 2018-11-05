2e9c6a8a451bc0253830379b1d370b12cbe1a120
JHoangSMU <hvdthong@gmail.com>
2018-11-05 14:52:09 +0800

    adding kNN

--- kNN.py
+++ kNN.py
@@ -2,6 +2,16 @@ from ultis import extract_commit, reformat_commit_code
 from extracting import extract_msg, extract_code
 
 
+def empty_info(message, code):
+    return [i for i in range(0, len(message)) if (message[i] == '' or code[i] == '')]
+
+
+def del_info(data, indexes):
+    for i in indexes:
+        del data[i]
+    return data
+
+
 if __name__ == '__main__':
     path_file = './data/newres_funcalls_words_jul28.out'
     commits = extract_commit(path_file=path_file)
@@ -10,6 +20,8 @@ if __name__ == '__main__':
 
     msgs = extract_msg(commits=commits)
     codes = extract_code(commits=commits)
+    print(len(empty_info(message=msgs, code=codes)))
+    print(empty_info(message=msgs, code=codes))
     print(len(msgs), len(codes))
     exit()
     print(len(commits))
