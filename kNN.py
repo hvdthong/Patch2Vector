396e02f4138db553d41f08cd1c5377dd7d70da5a
JHoangSMU <hvdthong@gmail.com>
2018-11-06 18:34:02 +0800

    adding kNN

--- kNN.py
+++ kNN.py
@@ -4,6 +4,8 @@ from sklearn.model_selection import KFold
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
+from nltk.translate.bleu_score import sentence_bleu
+from nltk.translate.bleu_score import SmoothingFunction
 
 
 def empty_info(message, code):
@@ -64,10 +66,41 @@ def kNN_model(tf_diff_code):
      range(0, tf_diff_test.shape[0])]
 
 
+def finding_topK(cosine_sim, topK):
+    cosine_sim = list(cosine_sim)
+    topK_index = list()
+    for i in range(topK):
+        max_ = max(cosine_sim)
+        index = cosine_sim.index(max_)
+        topK_index.append(index)
+        del cosine_sim[index]
+    return topK_index
+
+
+def finding_bestK(diff_trains, diff_test, topK_index):
+    diff_code_train = get_data_index(data=diff_trains, indexes=topK_index)
+    diff_code_train = [d.split() for d in diff_code_train]
+    diff_code_test = diff_test.split()
+    chencherry = SmoothingFunction()
+    scores = [sentence_bleu(references=[d], hypothesis=diff_code_test, smoothing_function=chencherry.method1) for d in
+              diff_code_train]
+    bestK = topK_index[scores.index(max(scores))]
+    return bestK
+
+
 def load_kNN_model(org_diff_code, tf_diff_code, ref_msg, topK):
+    org_diff_train, org_diff_test = org_diff_code
     tf_diff_train, tf_diff_test = tf_diff_code
+    ref_train, ref_test = ref_msg
     for i in range(tf_diff_test.shape[0]):
-        exit()
+        cosine_sim = np.loadtxt('./tf_cosine_similarity/test_' + str(i) + '.txt')
+        topK_index = finding_topK(cosine_sim=cosine_sim, topK=topK)
+        bestK = finding_bestK(diff_trains=org_diff_train, diff_test=org_diff_test[i], topK_index=topK_index)
+        train_msg, test_msg = ref_train[bestK], ref_test[i]
+        chencherry = SmoothingFunction()
+        blue_score = sentence_bleu(references=[train_msg.split()], hypothesis=test_msg.split(),
+                                   smoothing_function=chencherry.method1)
+        print(i, blue_score)
 
 
 if __name__ == '__main__':
@@ -88,7 +121,9 @@ if __name__ == '__main__':
     # ---------------------------------------------------------------------------------------------------------
     # ---------------------------------------------------------------------------------------------------------
 
-
-
-
-
+    # loading cosine similarity data and get the top K nearest neighbors, and calculate BLEU-score between diff-code
+    # Then we identify the nearest neighbor and get the reference message, we then calculate the BLEU-score between
+    # training data and test data based on the reference messages
+    # ---------------------------------------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------------------------------------
+    load_kNN_model(org_diff_code=org_diff_data, tf_diff_code=tf_diff_data, ref_msg=ref_data, topK=k_nearest_neighbor)
