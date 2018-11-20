f7d6e296c2f5620a26324587428d0efa3bc60e7a
JHoangSMU <hvdthong@gmail.com>
2018-11-20 13:23:42 +0800

    adding DCNN

--- main_DCNN.py
+++ main_DCNN.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
     input_option = read_args().parse_args()
     input_help = read_args().print_help()
 
-    # path_file = './data/newres_funcalls_words_jul28.out'
+    path_file = './data/newres_funcalls_words_jul28.out'
     # path_file = './data/small_newres_funcalls_words_jul28.out'
     commits = extract_commit(path_file=path_file)
     commits = reformat_commit_code(commits=commits, num_file=1, num_hunk=8,
