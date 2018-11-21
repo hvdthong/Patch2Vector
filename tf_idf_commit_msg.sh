c4abeba456d58bdb396d45992f7b45a8bd489e86
JHoangSMU <hvdthong@gmail.com>
2018-11-21 20:48:20 +0800

    adding sh files

---
+++ tf_idf_commit_msg.sh
@@ -0,0 +1,16 @@
+python tf_idf_commit_msg.py 0 5000 &
+python tf_idf_commit_msg.py 5000 10000 &
+python tf_idf_commit_msg.py 10000 15000 &
+python tf_idf_commit_msg.py 15000 20000 &
+python tf_idf_commit_msg.py 20000 25000 &
+python tf_idf_commit_msg.py 25000 30000 &
+python tf_idf_commit_msg.py 30000 35000 &
+python tf_idf_commit_msg.py 35000 40000 &
+python tf_idf_commit_msg.py 40000 45000 &
+python tf_idf_commit_msg.py 45000 50000 &
+python tf_idf_commit_msg.py 50000 55000 &
+python tf_idf_commit_msg.py 55000 60000 &
+python tf_idf_commit_msg.py 60000 65000 &
+python tf_idf_commit_msg.py 65000 70000 &
+python tf_idf_commit_msg.py 70000 75000 &
+python tf_idf_commit_msg.py 80000 80550 &
