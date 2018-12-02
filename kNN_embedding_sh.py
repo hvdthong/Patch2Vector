63821a4ec396b7f89a63c0b4bbb23102c3b99136
JHoangSMU <hvdthong@gmail.com>
2018-12-02 15:07:38 +0800

    calculate embedding

--- kNN_embedding_sh.py
+++ kNN_embedding_sh.py
@@ -10,10 +10,10 @@ if __name__ == '__main__':
 
     data = list()
     for i in range(start_epoch, end_epoch + 1):
-        print('python kNN_embedding.py -datetime ' + datetime + ' -embedding_dime ' + str(
+        print('python kNN_embedding.py -datetime ' + datetime + ' -embedding_dim ' + str(
             embedding_dim) + ' -filter_sizes ' + filter_sizes + ' -hidden_units ' + str(
-            hidden_units) + ' -num_filters ' + str(num_filters) + ' -start_epoch ' + str(i))
+            hidden_units) + ' -num_filters ' + str(num_filters) + ' -start_epoch ' + str(i) + ' &')
         data.append('python kNN_embedding.py -datetime ' + datetime + ' -embedding_dime ' + str(
             embedding_dim) + ' -filter_sizes ' + filter_sizes + ' -hidden_units ' + str(
-            hidden_units) + ' -num_filters ' + str(num_filters) + ' -start_epoch ' + str(i))
-    write_file(path_file=datetime + '.txt', data=data)
+            hidden_units) + ' -num_filters ' + str(num_filters) + ' -start_epoch ' + str(i) + ' &')
+    write_file(path_file=datetime + '.sh', data=data)
