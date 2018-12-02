a2dbcfd4ea0ecb456333f76f0e1206f6f9e498df
JHoangSMU <hvdthong@gmail.com>
2018-12-02 13:31:47 +0800

    create embedding

--- load_embedding_topwords.py
+++ load_embedding_topwords.py
@@ -75,8 +75,8 @@ if __name__ == '__main__':
     input_option = read_args().parse_args()
     input_help = read_args().print_help()
 
-    # path_file = './data/newres_funcalls_words_jul28.out'
-    path_file = './data/small_newres_funcalls_words_jul28.out'
+    path_file = './data/newres_funcalls_words_jul28.out'
+    # path_file = './data/small_newres_funcalls_words_jul28.out'
     commits = extract_commit(path_file=path_file)
     commits = reformat_commit_code(commits=commits, num_file=1, num_hunk=8,
                                    num_loc=10, num_leng=120)
