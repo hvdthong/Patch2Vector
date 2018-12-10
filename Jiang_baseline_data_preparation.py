74c986245100d1ac23964f658049566071962df4
hvdthong <hvdthong@gmail.com>
2018-12-10 16:07:51 +0800

    change the names

--- Jiang_baseline_data_preparation.py
+++ Jiang_baseline_data_preparation.py
@@ -4,12 +4,32 @@ from ultis import load_file
 def get_diff_data(commit, type_code):
     lines = list()
     for c in commit:
-
+        c = c.strip()
+        if c.strip() != '':
+            if type_code == 'added':
+                if c.startswith('+'):
+                    lines.append(c[1:].strip())
+                elif c.startswith('-'):
+                    # do nothing
+                    pass
+                else:
+                    lines.append(c)
+            elif type_code == 'removed':
+                if c.startswith('-'):
+                    lines.append(c[1:].strip())
+                elif c.startswith('+'):
+                    # do nothing
+                    pass
+                else:
+                    lines.append(c)
+    return lines
 
 
 def load_Jiang_code_data(pfile):
     data = load_file(path_file=pfile)
-    data = [d.split('<nl>') for d in data]
+    data_removed = [get_diff_data(commit=d.split('<nl>'), type_code='removed') for d in data]
+    data_added = [get_diff_data(commit=d.split('<nl>'), type_code='added') for d in data]
+    data = [{'added': a, 'removed': r} for a, r in zip(data_added, data_removed)]
     return data
 
 
